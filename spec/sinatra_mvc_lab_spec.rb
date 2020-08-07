38870c3dd103eeddf7cc993af81045d9ce8cc5ae
Tomas Perez <tomasitopg6@gmail.com>
2020-08-07 05:36:01 +0000

    Automatically backed up by Learn

--- spec/sinatra_mvc_lab_spec.rb
+++ spec/sinatra_mvc_lab_spec.rb
@@ -27,37 +27,37 @@
 #   #   end
 #   #
 #   # end
-# 
+#
 #   describe "POST '/piglatinize'" do
 #     before do
 #       post '/piglatinize', {
 #         "user_phrase"=> "Once upon a time and a very good time it was there was a moocow coming down along the road and this moocow that was coming down along the road met a nice little boy named baby tuckoo"
 #       }
 #     end
-# 
+#
 #     it "returns a 200 status code" do
 #       expect(last_response.status).to eq(200)
 #     end
-# 
+#
 #     it "displays the pig latinized phrase upon form submission" do
 #       expect(last_response.body).to include("Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay")
 #     end
 #   end
-# 
+#
 #   describe "POST '/piglatinize' again" do
 #     before do
 #       post '/piglatinize', {
 #         "user_phrase"=> "He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty four days now without taking a fish"
 #       }
 #     end
-# 
+#
 #     it "returns a 200 status code" do
 #       expect(last_response.status).to eq(200)
 #     end
-# 
+#
 #     it "displays the pig latinized phrase upon form submission" do
 #       expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
 #     end
 #   end
-# 
+#
 # end
